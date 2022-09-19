1f91b4d285823c80a0cc49de6644f8e875a09644
Alan <alandaunay1@gmail.com>
2022-09-19 17:33:07 +0200

    changement

--- dist/challenge.js
+++ dist/challenge.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log('hello');
+const prettyPrintWilder = (users) => {
+    users.map((user) => {
+        console.log(`${user.name} is ${user.age} years old`);
+    });
+};
+const wilders = [];
+const user1 = { name: "Pierre", age: 23 };
+const user2 = { name: "Paul", age: 33, birthday: "10/02/1990" };
+const user3 = { name: "Jacques", age: 25 };
+wilders.push(user1);
+wilders.push(user2);
+wilders.push(user3);
+prettyPrintWilder(wilders);
